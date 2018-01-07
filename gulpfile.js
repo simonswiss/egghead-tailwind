51eec9f2003bfc8e15a644fa283ee55fdcbc073f
Simon Vrachliotis <simon@simonswiss.com>
2018-01-07 13:34:59 +1100

    wip

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require("gulp");
 const postcss = require("gulp-postcss");
 const tailwindcss = require("tailwindcss");
+const purgecss = require("gulp-purgecss");
 const browserSync = require("browser-sync").create();
 
 const PATHS = {
@@ -13,10 +14,15 @@ gulp.task("css", () => {
   return gulp
     .src(PATHS.css)
     .pipe(postcss([tailwindcss(PATHS.config), require("autoprefixer")]))
+    .pipe(
+      purgecss({
+        content: [PATHS.dist + "*.html"]
+      })
+    )
     .pipe(gulp.dest(PATHS.dist));
 });
 
-gulp.task("serve", function() {
+gulp.task("serve", ["css"], function() {
   browserSync.init({
     server: "./",
     notify: false
